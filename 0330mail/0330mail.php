7d0f969422d0a0c19ec9b877a20ec4129e833ea6
kaisen0309 <113817802+kaisen0309@users.noreply.github.com>
2023-05-10 14:05:23 +0800

    Add files via upload

---
+++ 0330mail/0330mail.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+</head>
+
+<body>
+<form action="0330php.php" method="post">
+
+Mail to:<input type="email" name="myEmail"></br>
+Subject:<input type="text" name="mySubject"></br>
+content:</br><textarea col=30 row=30 name="myContent">
+</textarea><br/>
+<input type ="submit" value="send"></br>
+
+</form>
+</body>
+
+
+</html>
